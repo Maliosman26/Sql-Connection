ff03e1eb7c24810a083f90bea5850258428278bc
Maliosman26 <muhammetaliosmanozyurt@gmail.com>
2021-08-11 14:58:24 +0300

    Sql connection made

--- Personel_Kayit_Projesi/Form1.cs
+++ Personel_Kayit_Projesi/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient; //Sql komutlarını kullanabilmek için gerekli olan kütüphanedir
+using System.Data.SqlClient; //Sql komutlarını kullanabilmek için gerekli olan kütüphanedir.
 
 namespace Personel_Kayit_Projesi
 {
